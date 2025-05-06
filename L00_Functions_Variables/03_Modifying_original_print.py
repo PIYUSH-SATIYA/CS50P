3597eadb5b89c6cee57ce8c23650709792dab3bc
piyush-satiya <pbsatiya007@gmail.com>
2025-05-06 14:54:32 +0530

    Fixed typo

--- L00_Functions_Variables/03_Modifying_original_print.py
+++ L00_Functions_Variables/03_Modifying_original_print.py
@@ -3,7 +3,7 @@ name = input("Enter name: ")
 print("hello, ", end='')
 # As end is a keyword it will not be converted simply into a string
 # we just modified the original definition end='\n' to end="" means after the print function ends it will end by "" instead of \n
-#"" or '' any of them can be used but fid anyone
+#"" or '' any of them can be used but fix anyone
 print(name)
 
 print("hello, ", name, end='anything ', sep= '&' )
